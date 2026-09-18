dfca53605796078f869bb77be28206ff2f895029
Minenhle-56 <parkiesminenhle@gmail.com>
2026-09-18 12:29:38 +0200

    Add files via upload

---
+++ PoePart1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poe.part1;
+
+/**
+ *
+ * @author user
+ */
+public class PoePart1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
